ad440da5dae9af76bfd7b42522ffa956cacebe39
CarlosDosGit <chsilva1419@gmail.com>
2023-04-09 21:12:59 -0300

    Quase terminado

---
+++ js/Mercado.js
@@ -0,0 +1,76 @@
+Compras = new Array;
+Compras = [''];
+
+// Primeira ação
+function minhaFuncaoPera(){
+    const el = document.querySelector('#pera');
+    el.style.cssText =
+    'height: 80px;' +
+    'width: 80px;';
+}
+function minhaFuncaoMaca(){
+    const el = document.querySelector('#maça');
+    el.style.cssText =
+    'height: 80px;' +
+    'width: 80px;';
+}
+function minhaFuncaoBanana(){
+    const el = document.querySelector('#banana');
+    el.style.cssText =
+    'height: 80px;' +
+    'width: 80px;';
+}
+function botao(){
+    const el = document.querySelector('#botao');
+    el.style.cssText =
+    'height: 64px;' +
+    'width: 64px;';
+}
+// FIM
+
+// Segunda ação
+function PeraTWOFuncao(){
+    const el = document.querySelector('#pera');
+    el.style.cssText = 
+    'height: 70px;' +
+    'width: 70px;';
+}
+function MacaTWOFuncao(){
+    const el = document.querySelector('#maça');
+    el.style.cssText = 
+    'height: 70px;' +
+    'width: 70px;';
+}
+function BananaTWOFuncao(){
+    const el = document.querySelector('#banana');
+    el.style.cssText = 
+    'height: 70px;' +
+    'width: 70px;';
+}
+function botaoOne(){
+    const el = document.querySelector('#botao');
+    el.style.cssText =
+    'height: 60px;' +
+    'width: 60px;';
+}
+// FIM
+
+// Terceira ação
+function PeraTREEFuncao(){
+    Compras.push('<br>Pera');
+    document.getElementById("texto").innerHTML = Compras.join('');
+}
+function MacaTREEFuncao(){
+    Compras.push('<br>Maça');
+    document.getElementById("texto").innerHTML = Compras.join('');
+}
+function BananaTREEFuncao(){
+    Compras.push('<br>Banana');
+    document.getElementById("texto").innerHTML = Compras.join('');
+}
+// FIM
+
+function Remover(){
+    Compras.shift();
+    document.getElementById("texto").innerHTML = Compras.join('');
+}
